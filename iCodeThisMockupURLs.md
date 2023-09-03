adccf0826995292bfbbe50a1590e38048001f47d
DanielSegarra36 <danielsegarra36@gmail.com>
2023-09-03 04:00:43 +0000

    msg

--- iCodeThisMockupURLs.md
+++ iCodeThisMockupURLs.md
@@ -1,4 +1,4 @@
 [2023: Rent A Car](https://www.icodethis.com/_next/image?url=%2Fimages%2Fprojects%2Frent_a_car.png&w=3840&q=75)  
 [2023-08-31: Select Interest](https://www.icodethis.com/_next/image?url=%2Fimages%2Fprojects%2Fselect_interest.png&w=3840&q=75)  
 [2023-09-01: Social UI Kit](https://www.icodethis.com/_next/image?url=%2Fimages%2Fprojects%2Fsocial_ui_kit.png&w=3840&q=75)  
-[2023-09-02: Task Details](https://www.icodethis.com/_next/image?url=%2Fimages%2Fprojects%2Ftask_details.jpg&w=3840&q=75)  
+[2023-09-03: Task Details](https://www.icodethis.com/_next/image?url=%2Fimages%2Fprojects%2Ftask_details.jpg&w=3840&q=75)  
--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from bs4 import BeautifulSoup
 url = 'https://icodethis.com/app'
 
 # File to append URLs to
-urlsOnlyFile = 'iCodeThisMockupURLs.txt'
-markdownFile = 'iCodeThisMockupURLs.md'
+urls_only_file = 'iCodeThisMockupURLs.txt'
+markdown_file = 'iCodeThisMockupURLs.md'
 
 try:
   # Send an HTTP GET request to the URL
@@ -27,42 +27,58 @@ try:
       src_attribute = img_tag.get('src', '')
       alt_attribute = img_tag.get('alt', '')
 
+      # two images match our query, the first
+      # is a base64 img unrelated to the daily challenge
       if 'base64' not in src_attribute.lower():
         print('Found <img> tag without "base64" in src attribute')
         image_url = img_tag.get('src')
-        print(f'Image URL: icodethis.com{image_url}')
         full_url = f'https://www.icodethis.com{image_url}'
         print(f'Image URL: {full_url}')
 
-        # Open the file in append mode
-        with open(urlsOnlyFile, 'a') as urlFile:
+        # Send an HTTP GET request to the image URL
+        res = requests.get(full_url)
+        # Get the content of the image
+        image_content = res.content
+
+        # Regular expression pattern to find 'Daily Challenge Name'
+        pattern = r'image\?url=%2Fimages%2Fprojects%2F(.*?)\.jpg'
+
+        # Use re.search to find the match in the input string
+        match = re.search(pattern, image_url)
+
+        challenge_name = ''
+        timestamp = ''
+
+        # Check if a match was found
+        if match:
+          challenge_name = match.group(1)  # Extract the matched substring
+          print(f'challenge_name: {challenge_name}\n')
+
+          # Get the current date and time
+          current_datetime = datetime.datetime.now()
+
+          # Format the timestamp as a string
+          timestamp = current_datetime.strftime("%Y-%m-%d")
+        else:
+          print("No match found")
+
+        # Specify the file path where you want to save the image
+        new_image_file_path = f'./mockups/{timestamp}-{challenge_name}.jpg'
+
+        # DOWNLOAD IMAGE
+        with open(new_image_file_path, 'wb') as image_file:
+            image_file.write(image_content)
+
+        # SAVE IMAGE URL
+        with open(urls_only_file, 'a') as urlFile:
           # Append the URL to the file
           urlFile.write(full_url + '\n')
 
-        # Open the file in append mode
-        with open(markdownFile, 'a') as mdFile:
-          # Define the regular expression pattern to match the desired substring
-          pattern = r'image\?url=%2Fimages%2Fprojects%2F(.*?)\.jpg'
-
-          # Use re.search to find the match in the input string
-          match = re.search(pattern, image_url)
-
-          # Check if a match was found
-          if match:
-            extracted_string = match.group(1).replace(
-                '_', ' ').title()  # Extract the matched substring
-            print(f'extracted_string: {extracted_string}\n')
-
-            # Get the current date and time
-            current_datetime = datetime.datetime.now()
-
-            # Format the timestamp as a string
-            timestamp = current_datetime.strftime("%Y-%m-%d")
-
-            mdFile.write(f'[{timestamp}: {extracted_string}]({full_url})  \n')
-          else:
-            print("No match found")
-          # Append the URL to the file
+        # MARKDOWN DOC
+        with open(markdown_file, 'a') as mdFile:
+          challenge_name = challenge_name.replace(
+              '_', ' ').title()
+          mdFile.write(f'[{timestamp}: {challenge_name}]({full_url})  \n')
         break
 
       # print their attributes
